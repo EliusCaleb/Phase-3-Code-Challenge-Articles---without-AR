8699e603f76f471c50b6e865e3333b3efa9e3dc5
EliusCaleb <eliuswanyoro@gmail.com>
2022-10-29 13:27:53 +0300

    learning

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,54 @@
 class Article
+#     `Article#initialize(author, magazine, title)`
+#     - An article is initialized with an author as an Author object, a magazine as a Magazine object, and title as a string.
+#     - An article **cannot** change its author, magazine, or title after it is has been initialized.
+#   - `Article#title`
+#     - Returns the title for that given article
+#   - `Article.all`
+#     - Returns an array of all Article instances
+    attr_reader :author, :magazine,:title
+    @@all = []
+    def initialize(author,magazine,title)
+         @author = author,
+         @magazine = magazine,
+         @title = title
+         @@all << self
+    
+    end
+
+    # Article
+
+    # - `Article#author`
+    #   - Returns the author for that given article
+    # - `Article#magazine`
+    #   - Returns the magazine for that given article
+    def title
+        @title
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def author
+        self.class.all.each do|article,title|
+            if article.title == title
+                return author
+            end
+        end 
+    end
+
+    def magazine
+        self.class.all.each do |article,magazine|
+            if article.magazine == magazine
+               return magazine
+            end
+        end
+    end
 
 
 end
+
+
+ puts hello = Article.new( "Forbes")
+ puts hello.author
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,16 @@
 class Author
-  attr_accessor :name
-
-
+  #attr_accessor :name
+   attr_reader :name
+#    `Author#initialize(name)`
+#   - An author is initialized with a name, as a string.
+#   - A name **cannot** be changed after it is initialized.
+# - `Author#name`
+#   - Returns the name of the author
   def initialize(name)
     @name = name
-  
   end
-
-
+  
+  
 end
+puts musk = Author.new("Elon")
+puts musk.name
