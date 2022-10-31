e6f75c1cc2f8948c740a2c6cc50937190463bd58
EliusCaleb <eliuswanyoro@gmail.com>
2022-10-31 05:55:48 +0300

    Learning

--- solution.rb
+++ solution.rb
@@ -50,36 +50,38 @@ end
 
 #author
 class Author
-    @@all = []
-    attr_reader :name
-   
-   def initialize(name)
-     @name = name
-     @@all << self
-   end
- 
-   
-   def articles
-     Article.all.select{|article| article.author == self}
-   end
- 
-   def magazines
-     self.articles.collect{|articles| articles.magazine}.uniq
-   end
-   def self.all
-     @@all
-   end
- 
-   
-   def add_article(magazine, title)
-     Article.new( self, magazine,title )
-   end
-   
-   def topic_areas
-       self.magazines.collect{|magazine| magazine.category}.uniq
-   end
-   
- end
+   @@all = []
+   attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  
+  def articles
+    Article.all.select{|article| article.author == self}
+  end
+
+  def magazines
+    self.articles.collect{|articles| articles.magazine}.uniq
+  end
+  def self.all
+    @@all
+  end
+
+  
+  def add_article(magazine, title)
+    Article.new( self, magazine,title )
+  end
+  
+  def topic_areas
+      self.magazines.collect{|magazine| magazine.category}.uniq
+  end
+  
+end
+
+
  
  
  
